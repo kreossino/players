bee053ea43b8a269d7b367469239ce7b99aa1484
root <root@fenice.serverkreos.eu>
2016-07-23 12:21:19 +0200

    primo inserimento files di lingua

---
+++ languages/English/English_global.php
@@ -0,0 +1,10 @@
+<?php
+
+// Always use the format LAN_PLUGIN_{FOLDER}_{TYPE} to prevent conflicts. In this case "_BLANK" is the folder. 
+// This should contain the LANs used in the plugin.xml file. 
+
+define("LAN_PLUGIN_PLAYERS_NAME", "Players");
+define("LAN_PLUGIN_PLAYERS_DIZ", "test plugin");
+define("LAN_PLUGIN_PLAYERS_LINK", "Collegamento vuoto");
+define("LAN_PLUGIN_VOLLEY_NAME", "Players SirVolley");
+?>
---
+++ languages/Italian/Italian_global.php
@@ -0,0 +1,11 @@
+<?php
+
+// Bing-Translated Language file 
+// Generated for e107 v2.x by the Multi-Language Plugin
+// https://github.com/e107inc/multilan
+
+define("LAN_PLUGIN_PLAYERS_NAME", "Players");
+define("LAN_PLUGIN_PLAYERS_DIZ", "test plugin");
+define("LAN_PLUGIN_PLAYERS_LINK", "Collegamento vuoto");
+define("LAN_PLUGIN_VOLLEY_NAME", "Players SirVolley");
+?>
