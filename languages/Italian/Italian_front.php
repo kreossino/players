9575b7fb53367a9d37051d9acdcce4d80190924a
kreossino <kreos@chess-camper.it>
2016-07-23 12:33:18 +0200

    Create Italian_front.php
    primo file di lingua

---
+++ languages/Italian/Italian_front.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file
+ * Definition of Italian strings for area Frontend.
+ */
+define('LAN_PLAYERS_01', 'Nme e Congome');
+define('LAN_PLAYERS_02', 'Ruolo');
+define('LAN_PLAYERS_03', 'data di nascita');
+define('LAN_PLAYERS_04', 'altezza');
+define('LAN_PLAYERS_05', 'nazionalità');
+define('LAN_PLAYERS_06', 'numero maglia');
+define('LAN_PLAYERS_07', 'Facebook');
+define('LAN_PLAYERS_08', 'Twitter');
+define('LAN_PLAYERS_09', 'Incarico');
+
+?>
